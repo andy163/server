f18fc89f7fb02891244edb9c2a00a767edde1dff
andy.pang <andy.pang@hey900.com>
2016-11-09 15:08:14 +0800

    com.pangbohao.server

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-rm -rf /opt/common_api/nohup.out
+rm -rf /opt/server/nohup.out
 
-if [ -e "/var/run/common_api.pid" ]; then
-  pid=$(cat /var/run/common_api.pid)
+if [ -e "/var/run/server_api.pid" ]; then
+  pid=$(cat /var/run/server_api.pid)
   kill $pid
 fi
 
-cd /opt/common_api
+cd /opt/server/
 
 ulimit -n 10240
-nohup java -Xmx1g -DSERVER_ADDRESS="weixin.jh920.com" -DDEBUG="false" -jar /opt/common_api/common_api-1.0.jar &
+nohup java -Xmx1g -DSERVER_ADDRESS="pangbohao.com" -DDEBUG="false" -jar /opt/common_api/common_api-1.0.jar &
 echo $! > /var/run/common_api.pid
 
